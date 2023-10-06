00a5fc4a1425c5f6faaa65f193bf994211167c5a
OmarAmer-226 <138832756+OmarAmer-226@users.noreply.github.com>
2023-10-06 23:39:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// window.addEventListener('load',function(){
+// })    
+    // $(document).ready(function(){
+    //     $('.lds-ripple').fadeOut(2000,function(){
+    //         $('.loading').slideUp(2000)
+    //         $('body').css('overflow','visible')
+    //     })
+    // })
+
+
+
+// particlesJS.load('particles-js', 'particles.json', function() {
+//     console.log('callback - particles.js config loaded');
+// });
+
+//show the button on scroll
+// $(document).ready(function(){
+//     $(window).on("scroll", function(){
+//         if($(window).scrollTop() > 300 ){
+//             $("#scrollToTop").css("transform","translateY(0)");
+//         }
+//         else{
+//             $("#scrollToTop").css("transform","translateY(100px)");
+//         }
+//     });
+//     $("#scrollToTop").click(function(){
+//         $("html,body").animate({scrollTop:"0"},1000)
+//     })
+// });
